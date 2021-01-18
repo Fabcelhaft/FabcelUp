23957449490aaadb7febc56c51d4d837c4966cb3
Fabian Lemke <fabian.lemke@online.de>
2021-01-18 20:15:54 +0100

    :tada:

---
+++ src/main/kotlin/net/fabcelhaft/fabcelup/FabcelUpApplication.kt
@@ -0,0 +1,11 @@
+package net.fabcelhaft.fabcelup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FabcelUpApplication
+
+fun main(args: Array<String>) {
+	runApplication<FabcelUpApplication>(*args)
+}
---
+++ src/test/kotlin/net/fabcelhaft/fabcelup/FabcelUpApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.fabcelhaft.fabcelup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FabcelUpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
